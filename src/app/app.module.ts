7c7d44286f0446a3ec4c2239513d0e998ef7caac
anthonyassaf <62218418+anthonyassaf@users.noreply.github.com>
2020-07-15 22:59:31 +0300

    Assignment 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing/app-routing.module';
 import { AppComponent } from './app.component';
-
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar'; 
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { MenuComponent } from './menu/menu.component';
-
 import { MatListModule } from '@angular/material/list';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
 
-import { DishService } from './services/dish.service';
-import { PromotionService } from './services/promotion.service';
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
 import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
 import { ContactComponent } from './contact/contact.component';
 
+import { DishService } from './services/dish.service';
+import { PromotionService } from './services/promotion.service';
+import { LeaderService } from './services/leader.service';
 
 
 @NgModule({
@@ -49,8 +47,8 @@ import { ContactComponent } from './contact/contact.component';
   ],
   providers: [
     DishService,
-    PromotionService
-    
+    PromotionService,
+    LeaderService
   ],
   bootstrap: [AppComponent]
 })
